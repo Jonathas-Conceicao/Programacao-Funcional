f46298e04da59f33c2ec24a0ce36847ca2aa45f4
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2018-05-22 21:35:56 -0300

    Tips implemented

--- trabalho1/Justifica.hs
+++ trabalho1/Justifica.hs
@@ -4,12 +4,12 @@ justifica :: String -> String
 justifica = error "Todo: Implement justifica"
 
 separaLinhas :: String -> [String]
-separaLinhas = error "Todo: Implement separaLinhas"
+separaLinhas = lines
 tamanhoMaiorLinha :: [String]-> Int
-tamanhoMaiorLinha = error "Todo: Implement tamanhoMaiorLinha"
+tamanhoMaiorLinha = (foldr (max) 0) . (map length)
 separaPalavras :: String -> [String]
-separaPalavras = error "Todo: Implement separaPalavras"
+separaPalavras = words
 insereEspacos :: Int -> String -> String
-insereEspacos = error "Todo: Implement insereEspacos"
+insereEspacos = (++) . (flip replicate ' ')
 
 textoEx1 = "RUBIÃO fitava a enseada -- eram oito horas da manhã.\nQuem o visse com os polegares metidos no cordão do chambre à janela de uma\ngrande casa de Botafogo cuidaria que ele admirava aquele pedaço de água\nquieta mas em verdade vos digo que pensava em outra coisa.\nCotejava o passado com o presente. Que era há um ano?\nProfessor. Que é agora? Capitalista! Olha para si para as chinelas\n(umas chinelas de Túnis que lhe deu recente amigo Cristiano Palha) para a casa\npara o jardim para a enseada para os morros e para o céu e tudo desde as chinelas\naté o céu tudo entra namesma sensação de propriedade."
